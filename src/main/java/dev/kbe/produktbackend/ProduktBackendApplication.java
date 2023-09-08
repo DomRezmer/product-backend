96806a980965666dc33574c03fa7e7a50851a1f1
DomRezmer <dom.rezmer@gmail.com>
2023-09-08 13:24:29 +0200

    springboot init

---
+++ src/main/java/dev/kbe/produktbackend/ProduktBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.kbe.produktbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduktBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduktBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kbe/produktbackend/ProduktBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kbe.produktbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduktBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
